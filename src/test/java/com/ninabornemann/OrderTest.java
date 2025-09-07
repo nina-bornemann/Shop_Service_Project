596b9b4d180e08918cbbbaee73961f934d3f6534
Nina Bornemann <nina.bornemann@freenet.de>
2025-09-07 16:05:16 -0700

    add assertj test and parameterized test for orders

--- src/test/java/com/ninabornemann/OrderTest.java
+++ src/test/java/com/ninabornemann/OrderTest.java
@@ -1,7 +1,11 @@
 package com.ninabornemann;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import static org.assertj.core.api.Assertions.*;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,16 +15,20 @@ class OrderTest {
     @Test
     void sumPriceOfOrders_isEqual() {
         Order o = new Order("5", Map.of(new Product("2", "Pancake", 2.5, 7), 4));
-        double expected = 2.5 * 4;
         double actual = o.sumPriceOfOrders();
-        assertEquals(expected, actual);
+        assertThat(actual).isEqualTo(10);
     }
 
     @Test
     void sumPriceOfOrders_isAlsoEqual() {
         Order o = new Order("5", Map.of(new Product("2", "Pancake", 2.5, 7), 3));
-        double expected = 2.5 * 3;
-        double actual = o.sumPriceOfOrders();
-        assertEquals(expected, actual);
+        assertThat(o.sumPriceOfOrders()).isEqualTo(7.5);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"5,2.5,2", "12,3,4", "7.5,1.5,5"})
+    void sumPriceOfOrders_isEqualAgain(double expected, double price, int quantity) {
+        Order o = new Order("5", Map.of(new Product("2", "Pancake", price, 7), quantity));
+        assertThat(o.sumPriceOfOrders()).isEqualTo(expected);
     }
 }
